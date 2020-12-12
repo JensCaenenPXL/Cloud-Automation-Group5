6ab3612990f6bec8cabab9b90f5e036d7190a0d7
Thomas Venken <11700525@student.pxl.be>
2020-12-12 14:49:14 +0100

    module testing

--- main.tf
+++ main.tf
@@ -6,8 +6,7 @@ module "module_one" {
   source = ".\\module_one"
 }
 
- module "module_two" {
-   source = ".\\module_two"
-   depends_on = [module.module_one.module_one,]
-}
+// module "module_two" {
+//   source = ".\\module_two"
+//}
 
