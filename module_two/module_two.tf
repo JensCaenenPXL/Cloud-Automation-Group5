16bcd2984f265cfb7e1331adf8f35ea9b0439574
Thomas Venken <11700525@student.pxl.be>
2020-12-13 14:00:05 +0100

    launch template

--- module_two/module_two.tf
+++ module_two/module_two.tf
@@ -4,16 +4,16 @@
 
 variable "aws_access_key" {
   type    = string
-  default = "ASIA5547CE7RMHGACBPD"
+  default = "ASIA5547CE7RKGJABF7T"
 }
 variable "aws_secret_key" {
   type    = string
-  default = "aUFw2Qo7J9RJqF91jE6rE8B6fQbWnOWOstb/TzoP"
+  default = "Qd8dCobuqW3Bxfxsjr40ocp4JLm11XzlGYYxJWlm"
 }
 
 variable "aws_session_token" {
   type    = string
-  default = "FwoGZXIvYXdzEB8aDDYxIaEwnkGYBEcJFCKuAaVXec6VG6mdbyMGLAc9LRY6wKvzJ27oMW0PGatBhl/eE8gGZmAz4rhzWSpue9j66ylBUZAGbpP7a0MNzndrno77kyezANH9J0vxVWbDB03e26020mtX2N5I/Vn4lh7fNCwUfZnbLu/GUmvXwabj1r47yAj9q8JLr0JwfHRZk2ok4VVK6+fT9kVSyCzasPnXGY6qMnaGlq/DX5fNVuDvNXP8Qq3kI26Rw1AsCc6sYyjLg9P+BTItlsgxE2+Vwr1yVT7boRZRULXmpOpqdiwjI8AqIgrbpB5DFS/ef/fwDpVADaqj"
+  default = "FwoGZXIvYXdzEDQaDM+D+3kbuSQgT3xa8CKuAcg3x801g9RH//g+KFkI1FeT80c7oSfS4b2mVHhzgD+wHI8v4xMYOM7ZgqcfXxs2CVww6LUPB/ePQPrqqfmPqNwgjVM2/4HVL3+cguvA+6AOyfQViIb8HRp3olNlBPTkShr1AKlZgv9Y9l4zJoNInrBHZX+KcQwsvzrG+fUm+HBl5HhubW1KmkUzgiwOJKDOzFYHonUGr+MxdxxNh1/C0wj1bQxGsCBViFBwKq7Q9yiA4tf+BTItNDwmaH6zgiMx5F9fqhoE+hp3yH7ztZ21Ys+b5dTUbC/NH9YirfyrXj35uu6o"
 }
 
 variable "private_key_path" {
@@ -54,3 +54,21 @@ data "aws_ami" "aws-linux" {
 #####################################################################
 # RESOURCES
 #####################################################################
+
+resource "aws_launch_template" "webserver_launch_template" {
+  name_prefix   = "webserver_launch_template"
+  image_id      = aws_ami.aws-linux.id
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "webserver_autoscaling_group" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 2
+  max_size           = 3
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.webserver_launch_template.id
+    version = "$Latest"
+  }
+}
